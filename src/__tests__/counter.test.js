a2f55550d098f7b8a4470d2d3c62fc9f72b07960
Matthew_Heyert <heyerthb@gmail.com>
2019-11-19 16:04:24 -0800

    wrote tests for up/down counters and render

--- src/__tests__/counter.test.js
+++ src/__tests__/counter.test.js
@@ -2,19 +2,30 @@ import React from 'react';
 import Enzyme, {shallow, mount} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import renderer from 'react-test-renderer';
-import Counter from '../src/components/counter/counter';
+import Counter from '../components/counter/counter';
 
 Enzyme.configure({adapter: new Adapter()});
 
 describe ('Testing our Counter component', () => {
-  it ('Counter changes state on Click', () => {
+  it ('Up counter increases counter', () => {
     let app = mount (<Counter/>);
-    let button = app.find('button');
-    expect(button).toBeDefined();
+    let upClick = app.find('.up');
+    expect(upClick).toBeDefined();
     expect(app.state('count')).toBe(0);
-    button.simulate('click');
+    upClick.simulate('click');
     expect(app.state('count')).toBe(1);
   })
+
+  it ('Down counter decreases counter', () => {
+    let app = mount (<Counter/>);
+    let downClick = app.find('.down');
+    expect(downClick).toBeDefined();
+    expect(app.state('count')).toBe(0);
+    downClick.simulate('click');
+    expect(app.state('count')).toBe(-1);
+  })
+
+
 
   it('render correctly', () => {
     const renderTree = renderer.create(<Counter/>).toJSON();
